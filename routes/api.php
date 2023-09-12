827f985598a6377edf123c0ad4e772cbb1ab07c3
Rotimiiam <rotimiiam@gmail.com>
2023-09-12 18:32:17 +0100

    new

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-Route::get('/api', [ApiController::class, 'index']);
-Route::get('/api/{id}', [ApiController::class, 'show']);
-Route::post('/api/{name}', [ApiController::class, 'store']);
-Route::put('/api/{id}', [ApiController::class, 'update']);
-Route::delete('/api/{id}', [ApiController::class, 'destroy']);
+Route::get('/', [ApiController::class, 'index']);
+Route::get('/{id}', [ApiController::class, 'show']);
+Route::post('/{name}', [ApiController::class, 'store']);
+Route::put('/{id}', [ApiController::class, 'update']);
+Route::delete('/{id}', [ApiController::class, 'destroy']);
 
 
 // Additional routes can be defined here if needed.
